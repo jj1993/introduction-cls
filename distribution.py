d3ff8fef15cc95b365c8777297a92c477b7f606c
krishnakanthsasi <krishnakanthsasi@gmail.com>
2016-10-09 17:53:41 +0200

    Just a basic set of commands. Code will be updated with soon

---
+++ distribution.py
@@ -0,0 +1,29 @@
+# This file explores the distribution of food
+# Food distribution is dependent upon
+# Climate
+# Availability of domesicatable plans
+# Suitable Terrain
+# Lack of other food sources
+
+#Food production is correlated to
+# Livestock
+
+
+
+#Step
+# Initialization
+# Wildlife Distribution
+
+# Process
+# As the abundance withers away, Ratio of Hunter gathers/ settlers decreases. 
+# Favorable regions start developing food. 
+# Animals are domesticated
+# Further increase in the production
+# Spread happens where ever favorable
+# Population and population density increases in "food centres"
+# Population Correlated to Development, and Immunity
+
+#Notes
+#Dry Climate Favored(Helped with Storage)
+# Last Ice Age ended at 11K BC
+# Mega Fauna Extinction
